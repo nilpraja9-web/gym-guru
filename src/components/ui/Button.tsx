754d5263e651b61078d0393b1700b6abeb9cf79d
Niles-Prazapate <nil.praja9@gmail.com>
2026-03-26 13:29:49 +0545

    minor fix

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -41,4 +41,4 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   },
 );
 
-Button.displayName = "Button";git pull origin main --rebase --allow-unrelated-histories
+Button.displayName = "Button";
